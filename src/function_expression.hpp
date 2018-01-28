115a7f5ea326774a5ec71efcbb0ba07c09ba4ed3
Lukasz Maciejewski <lukasz.m.maciejewski@gmail.com>
2018-01-28 19:56:44 +0100

    initial

---
+++ src/function_expression.hpp
@@ -0,0 +1,31 @@
+#ifndef DRAKMOOR_FUNCTION_EXPRESSION
+#define DRAKMOOR_FUNCTION_EXPRESSION
+
+class value
+{
+
+};
+
+class constant : public value
+{
+
+};
+
+class placeholder : public value
+{
+
+};
+
+class compound : public value
+{
+
+};
+
+class expression
+{
+    // aggregate values and op?
+};
+
+
+
+#endif // DRAKMOOR_FUNCTION_EXPRESSION
